cb1b7e9ebd0c3ee40506044a4aef2bdeabd9e2ad
FrankFrankWei <yanzhifei25@163.com>
2018-09-18 12:44:09 +0800

    re organize

---
+++ ModelImport/ImportBase.cs
@@ -0,0 +1,23 @@
+﻿/******************************************************************
+** auth: wei.huazhong
+** date: 9/18/2018 12:03:27 PM
+** desc:
+******************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelImport
+{
+    public class ImportBase
+    {
+        public ImportBase()
+        { }
+        public int Line { get; set; }
+        public bool IsError { get; set; }
+
+    }
+}
---
+++ ModelImport/SampleImport.cs
@@ -0,0 +1,27 @@
+﻿/******************************************************************
+** auth: wei.huazhong
+** date: 9/18/2018 12:00:21 PM
+** desc:
+******************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelImport
+{
+    public class SampleImport : ImportBase
+    {
+        public SampleImport()
+        { }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public float Height { get; set; }
+        public double Money { get; set; }
+        public bool IsMale { get; set; }
+    }
+}
