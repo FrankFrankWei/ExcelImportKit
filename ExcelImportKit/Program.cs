7f2a0a28dc00be90c67471121bf524908c4f7731
FrankFrankWei <yanzhifei25@163.com>
2018-09-19 17:59:28 +0800

    Revert " import service impl generic version"
    This reverts commit bda80618367585f0b6156c86b305094c7c4c9ba0.

--- ExcelImportKit/Program.cs
+++ ExcelImportKit/Program.cs
@@ -1,5 +1,4 @@
 ﻿using ExcelService;
-using ModelImport;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -15,7 +14,7 @@ namespace ExcelImportKit
         {
             var fs = new FileStream(@"F:\\sampleImport.xlsx", FileMode.Open);
             IList<ImportError> errors = new List<ImportError>();
-            var importList = new SampleImportService<SampleImport>().GetParsedPositionImport(fs, errors);
+            var importList = new SampleImportService().GetParsedPositionImport(fs, errors);
 
             if (errors.Count > 0)
                 errors.ToList().ForEach(item => Console.WriteLine($"{item.Line} - {item.ErrorMsg}"));
